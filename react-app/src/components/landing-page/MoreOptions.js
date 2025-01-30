4ae25305692ff9306b6b32fe7f11f5b3d4cd37d7
Dillon <dfcross18@gmail.com>
2025-01-30 13:49:41 -0700

    changed width of overall content, increased font size and margins for medium sized devices

--- react-app/src/components/landing-page/MoreOptions.js
+++ react-app/src/components/landing-page/MoreOptions.js
@@ -22,8 +22,8 @@ function MoreOptions() {
     return (
         <div className="w-full">
             <div className=" mt-3 w-full">
-                <p className=" text-sm mb-1">Select Genre</p>
-                <div className="border border-slate-300 rounded-md text-sm px-2 py-1 w-2/3 flex items-center">
+                <p className=" text-sm mb-1 md:text-base">Select Genre</p>
+                <div className="border border-slate-300 rounded-md text-sm px-2 py-1 w-2/3 flex items-center md:text-base">
                     <p>Random</p>
                     <div className=" ml-auto mr-2">
                         <IoIosArrowDown />
@@ -32,9 +32,9 @@ function MoreOptions() {
             </div>
 
             <div className=" mt-3 w-full">
-                <p className=" text-sm mb-1">Main Characters Name</p>
+                <p className=" text-sm mb-1 md:text-base">Main Characters Name</p>
                 <div className="flex items-center gap-2 relative">
-                    <input type="text" placeholder="Enter name..." className="border border-slate-300 rounded-md text-sm px-2 py-1 w-2/3 outline-none bg-stone-50"/>
+                    <input type="text" placeholder="Enter name..." className="border border-slate-300 rounded-md text-sm px-2 py-1 w-2/3 outline-none bg-stone-50 md:text-base"/>
                     <div className=" text-lg cursor-pointer" 
                         onClick={() => setShowNameInfo(!showNameInfo)} 
                         ref={nameInfoRef}
@@ -52,7 +52,7 @@ function MoreOptions() {
             </div>
 
             <div className=" mt-3 w-full">
-                <p className=" text-sm mb-1">Furigana</p>
+                <p className=" text-sm mb-1 md:text-base">Furigana</p>
                 <div className={`transition ${furigana ? `bg-teal-700` : `bg-stone-400`} duration-500 rounded-full p-1 w-12 flex cursor-pointer`} 
                     onClick={() => setFurigana(!furigana)}>
                     <div className={`h-5 w-5 rounded-full bg-stone-50 transition duration-500 ${furigana ? ` translate-x-full` : `translate-x-0`}`}></div>
