d9b8802583287dfaa86365d4bc950245cb70c012
Dillon <dfcross18@gmail.com>
2025-02-15 14:01:36 -0700

    made story generation its own component, taken from landing page

--- react-app/src/components/landing-page/LandingPage.js
+++ react-app/src/components/landing-page/LandingPage.js
@@ -1,16 +1,9 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { useAudioValues } from "../../contexts/AudioValuesContext";
-
-import DOMPurify from 'dompurify';
-import { marked } from 'marked';
+import { useState, useEffect } from "react";
 
 import MoreOptions from "./MoreOptions";
 import DifficultyOptions from "./DifficultyOptions";
 import LengthOptions from "./LengthOptions";
-import GenerateAudio from "./GenerateAudio";
-
-import { generateAudio } from "./GenerateAudio";
+import StoryGeneration from "./StoryGeneration";
 
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
@@ -32,10 +25,6 @@ function LandingPage() {
     const [showMoreOptions, setShowMoreOptions] = useState(false)
     const [difficulty, setDifficulty] = useState("")
     const [length, setLength] = useState("")
-    const [response, setResponse] = useState("こんにちは！")
-    const [loadingPrompt, setLoadingPrompt] = useState(false)
-
-    const audioValues = useAudioValues()
 
     const levels = ["PRE-N5 (~300-500 most common words)", "N5", "N4"]
 
@@ -52,32 +41,6 @@ function LandingPage() {
             setLength(settings.length)
         }
     }, [])
-
-
-    function generateStory() {
-
-        setResponse('')
-        setLoadingPrompt(true)
-        
-        fetch(`${process.env.REACT_APP_API_BASE_URL}/story-generator/generate-story/`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                story_settings: JSON.parse(localStorage.getItem('storySettings'))
-            })
-        })
-        .then(res => res.json())
-        .then(data => {
-            setLoadingPrompt(false)
-            let parsed_response = marked.parse(data.response)
-            const clean_response = DOMPurify.sanitize(parsed_response)
-            setResponse(clean_response)
-            generateAudio(clean_response, audioValues.setController, audioValues.controller, audioValues.setAudioURL, audioValues.setIsLoading, audioValues.isLoading)
-            console.log(clean_response)
-        })
-    }
     
     return (
         <div className="flex flex-col items-center">
@@ -131,37 +94,7 @@ function LandingPage() {
 
                 </div>
 
-                <button className="mt-12 self-start">
-                    <GenerateAudio response={response}/>
-                </button>
-
-                <button 
-                    className="bg-teal-700 text-stone-50 rounded-md w-full py-1 md:py-2 mt-6 md:text-lg font-semibold hover:bg-teal-800"
-                    onClick={() => {
-                        if (!loadingPrompt) {
-                            generateStory()
-                            audioValues.setAudioURL('')
-                        }
-                    }}>
-                        Generate Story
-                </button>
-
-                
-                <p 
-                    className=" bg-stone-50 self-start mt-4 text-lg md:text-2xl w-full h-full mb-28"
-                    dangerouslySetInnerHTML={{__html: response}}>
-                        
-                </p>
-                
-                {loadingPrompt && 
-                    <div role="status">
-                        <svg aria-hidden="true" className="w-12 h-12 text-gray-200 animate-spin dark:text-gray-300 fill-teal-700" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                        </svg>
-                        <span className="sr-only">Loading...</span>
-                    </div>
-                }
+                <StoryGeneration/>
 
             </div>
         </div>
